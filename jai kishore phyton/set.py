6be3e77f93425a480e28f9bf5b07c76fa409cfa7
Sangeethasankar <sangeethasankar474@gmail.com>
2024-07-02 13:55:10 +0530

    sets

---
+++ jai kishore phyton/set.py
@@ -0,0 +1,119 @@
+#set
+# apples={"green","red","white"}
+# print(apples)
+
+# fruits={"avacado","orange","apple","banana","orange"}
+# print(fruits)
+
+# colour={"green,'orange","yellow","black"}
+# print(type(colour))
+# print(len(colour))
+
+# my_set={True,False,2,1,0,"miniso"}
+# print(my_set)
+
+#access
+
+# jungle={"lion","tiger","elephant"}
+# for i in jungle:
+#    print(i).
+
+# colour={"green,'orange","yellow","black","red"}
+# print("red"in colour)
+
+# colour={"green,'orange","yellow","black"}
+# print("red" not in colour)
+
+#add
+
+# Name={"jingle bell","ike","high on river"}
+# Name.add("runaway maze")
+# print(Name)
+
+# colour1={"green,'orange","yellow","black"}
+# colour2={"red","humpy"}
+# colour1.update(colour2)
+# print(colour1)
+
+# thisset={"john","jigle","zizuka"}
+# thisset.remove("john")
+# print(thisset)
+
+# thisset={"john","jigle","zizuka"}
+# thisset.discard("john")
+# print(thisset)
+
+# thisset={"john","jigle","zizuka"}
+# thisset.clear()
+# print(thisset)
+
+# thisset={"john","jigle","zizuka"}
+# del thisset
+# print(thisset)
+
+#loopset
+
+# colour={"green,'orange","yellow","black","red"}
+# for i in colour:
+#     print(i)
+
+#join sets
+
+#set1={"1,2,3,4"}
+# set2={"apple","mango"}
+# set3=set1.union(set2)
+# print(set3)
+
+# set1={"1,2,3,4"}
+# set2={"apple","mango"}
+# set3={2,674,5,2,4,}
+# set4=set1.union(set2,set3)
+# print(set4)
+
+# set1={"1,2,3,4"}
+# set2={"apple","mango"}
+# set1.update(set2)
+# print(set1)
+
+# set1={"micro","organism","data science"}
+# set2={"micro","science"}
+# set1.intersection(set2)
+#  print(set1)
+
+# set1={1,2,3,1,2,3,11,11233}
+# set2={"gag",'knd',"flower"}
+# set1.difference(set2)
+# print(set1)
+
+# set1={"movies","games","art"}
+# set2={"king","tiger"}
+# set3=set1.isdisjoint(set2)
+
+# print(set3)
+
+# set1={"movies","games","art",1,2,233}
+# set2={"king","tiger",1,2}
+# set3=set1.issubset(set2)
+# print(set3)
+
+# set1={"movies","games","art",1,2,233}
+# set2={"king","tiger",1,2}
+# set3=set1.issuperset(set2)
+# print(set3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
