5cbdf3331b273cc1cfc600ea143c8d668c16d3d1
Sangeethasankar <sangeethasankar474@gmail.com>
2024-07-09 13:44:56 +0530

    exception

---
+++ exception/exception.py
@@ -0,0 +1,21 @@
+# try:
+#     number = float(input("Enter a number: "))
+#     reciprocal = 1 / number
+#     print(f"The reciprocal of {number} is {reciprocal}")
+# except ValueError:
+#     print("Invalid input. Please enter a valid number.")
+# except ZeroDivisionError:
+#     print("Error: Division by zero. Please enter a non-zero number.")
+
+
+
+# try:
+#     num = float(input("Enter a number: "))
+#     reciprocal = 1 / num
+#     print(f"The reciprocal of {num} is {reciprocal}")
+# except ValueError:
+#     print("Invalid input.")
+# except ZeroDivisionError:
+#     print("Error: Division by zero.")A
+# except Exception as e:
+#     print(f"Error: {e}")
